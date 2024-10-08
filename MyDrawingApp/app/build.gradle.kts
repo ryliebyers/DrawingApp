274462a7e1fae706866e4a4cb6000c04ea14861a
mina <minaakbarislc@gmail.com>
2024-10-07 22:43:50 -0600

    MyDrawingApp - MA - Oct7@10:43 PM - Added Unit Tests

--- MyDrawingApp/app/build.gradle.kts
+++ MyDrawingApp/app/build.gradle.kts
@@ -57,6 +57,16 @@ android {
 
 dependencies {
 
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.0")
+    testImplementation("io.mockk:mockk:1.12.0")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.0.5")
+
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.0.5")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.0.5")
+
+
+
 
 
 
